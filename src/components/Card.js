ee8035dd2093c3614e7532d7b44c3d6593741b81
prawin <84778635+prawin10-mech@users.noreply.github.com>
2023-04-18 09:41:32 +0000

    Initial commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,36 +3,45 @@ import axios from "axios";
 import "./Card.css";
 
 const Card = ({ card }) => {
-  const [balance, setBalance] = useState(null);
+  const [balance, setBalance] = useState(false);
 
   const fetchBalance = async () => {
     try {
-      const data = await fetch(
-        `https://${card.backend}.herokuapp.com/stats/view`,
-        {
-          method: "GET",
-          mode: "no-cors",
-        }
+      const { data } = await axios.get(
+        `https://${card.backend}.herokuapp.com/stats/view`
       );
       console.log(data);
+      setBalance(data.balance); // assuming that the server returns a JSON object with a 'balance' property
     } catch (err) {
-      console.log(err);
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
     }
   };
+
   useEffect(() => {
-    fetchBalance();
-  });
+    if (
+      card.backend === "callcenterbackend1" ||
+      card.backend === "callcenterbackend4"
+    ) {
+      setBalance(true);
+    }
+  }, [card.backend]);
+
   return (
     <div className="card">
-      <p>
-        <a href={`https://${card.backend}.herokuapp.com/stats/view`}>
-          {card.backend}
-        </a>
-      </p>
-      <p>
-        <a href={`https://${card.frontend}.herokuapp.com`}>{card.frontend}</a>
-      </p>
-      {<p>Balance: {balance}</p>}
+      {balance && (
+        <p>
+          <a href={`https://${card.backend}.herokuapp.com/stats/view`}>
+            {card.backend}
+          </a>
+        </p>
+      )}
+      {balance && (
+        <p>
+          <a href={`https://${card.frontend}.herokuapp.com`}>{card.frontend}</a>
+        </p>
+      )}
     </div>
   );
 };
