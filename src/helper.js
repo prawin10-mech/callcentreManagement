4a84c51dddb95b847cd6d871c8da9a3b69375085
prawin <84778635+prawin10-mech@users.noreply.github.com>
2023-04-29 04:56:11 +0000

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,9 @@
 export const cards = [
   //updated the data
-  { id: 1, backend: "callcenterbackend1", frontend: "brianscenter" },
+  { id: 1, backend: "callcenterbackend1", frontend: "richardpcentre" },
   // { id: 2, backend: "callcenterbackend2", frontend: "null" },
   // { id: 3, backend: "callcenterbackend3", frontend: "null" },
-  { id: 4, backend: "callcenterbackend4", frontend: "richardpcentre" },
+  { id: 4, backend: "callcenterbackend4", frontend: "brianscenter" },
   // { id: 5, backend: "callcenterbackend5", frontend: "null" },
   // { id: 6, backend: "callcenterbackend6", frontend: "null" },
 ];
